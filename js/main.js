4b4da185f04c4ba0ac7246eb170fc4264a3f6e35
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-14 18:23:25 +0100

    Js-Simon

--- js/main.js
+++ js/main.js
@@ -32,14 +32,7 @@ function generateRandomList (x, y) {
 // invoco la funzione
 generateRandomList(1, 100);
 
+
 // stampo in html il risultato di random list
 numberContainerEl.innerHTML = randomList;
 
-setTimeout(timer, 30000);
-
-function timer () {
-    numberContainerEl.innerHTML = "";
-    prompt("inserisci il primo numero della serie");
-}
-
-timer ();
