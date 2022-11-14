dd38104001637c986d9bc7a98ef31e60a69a0fc6
TomasBelfonte <tomas.belfonte@gmail.com>
2022-11-14 17:59:27 +0100

    Js-Simon

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// assegno e definisco le variabili
+let randomList = [];
+const numberContainerEl = document.querySelector (".number-container");
+
+
+// genero un insieme di cinque numeri casuali
+function generateRandomNumber ( min, max ) {
+    return Math.floor( Math.random() * ( max - min + 1 ) ) + min;
+}
+
+function generateRandomList (x, y) {
+    
+    randomList = [];
+
+
+    while ( randomList.length < 5 ) {
+
+        const randomNum = generateRandomNumber( x, y );
+    
+        if ( !randomList.includes( randomNum ) ) {
+
+        randomList.push( randomNum );
+        }
+
+    }
+
+    console.log(randomList);
+    
+    return randomList;
+}
+
+// invoco la funzione
+generateRandomList(1, 100);
+
+// stampo in html il risultato di random list
+numberContainerEl.innerHTML = randomList;
+
+setTimeout(timer, 30000);
+
+function timer () {
+    numberContainerEl.innerHTML = "";
+    prompt("inserisci il primo numero della serie");
+}
+
+timer ();
